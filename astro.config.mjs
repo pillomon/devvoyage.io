edeca37e5a8acc1946f47b2b0aaf20f79daf26d7
SUNG PILLSANG <schmidtiana95@gmail.com>
2024-04-03 23:18:06 +0900

    feat: 문서 작성 중

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,25 +7,10 @@ export default defineConfig({
   integrations: [tailwind()],
   markdown: {
     remarkPlugins: [remarkReadingTime],
+    syntaxHighlight: 'shiki',
     shikiConfig: {
-      // Choose from Shiki's built-in themes (or add your own)
-      // https://shiki.style/themes
       theme: 'dracula',
-      // Alternatively, provide multiple themes
-      // https://shiki.style/guide/dual-themes
-      themes: {
-        light: 'github-light',
-        dark: 'github-dark',
-      },
-      // Add custom languages
-      // Note: Shiki has countless langs built-in, including .astro!
-      // https://shiki.style/languages
-      langs: [],
-      // Enable word wrap to prevent horizontal scrolling
       wrap: true,
-      // Add custom transformers: https://shiki.style/guide/transformers
-      // Find common transformers: https://shiki.style/packages/transformers
-      transformers: [],
     },
   },
 });
--- src/constants/styles.ts
+++ src/constants/styles.ts
@@ -1,2 +1,1 @@
 export const LAYOUT_MAX_WIDTH_SIZE = 'max-w-[624px]';
-export const LAYOUT_PADDING_X_SIZE = 'px-[20px]';
