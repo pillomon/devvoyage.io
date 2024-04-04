68126e7c8cdc7cb461eb705614eedd0b6637b616
SUNG PILLSANG <schmidtiana95@gmail.com>
2024-04-04 23:02:03 +0900

    style: blog 목록, 상세 maxWidth 변경

--- src/constants/styles.ts
+++ src/constants/styles.ts
@@ -1,1 +1,6 @@
-export const LAYOUT_MAX_WIDTH_SIZE = 'max-w-[624px]';
+export const BLOG_LIST_MAX_WIDTH_SIZE = 'max-w-[992px]';
+export const BLOG_DETAIL_MAX_WIDTH_SIZE = 'max-w-[624px]';
+export enum LayoutType {
+  BLOG_LIST = 'blogList',
+  BLOG_DETAIL = 'blogDetail',
+}
