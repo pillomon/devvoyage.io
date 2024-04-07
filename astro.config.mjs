68f748548ed748cca895bc74833b23c3afba4c22
SUNG PILLSANG <schmidtiana95@gmail.com>
2024-04-08 00:41:16 +0900

    feat: astro 환경 구성

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
 import { defineConfig } from 'astro/config';
 import { remarkReadingTime } from './remark-reading-time.mjs';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), react({ include: ['**/react/*'] })],
   markdown: {
     remarkPlugins: [remarkReadingTime],
     syntaxHighlight: 'shiki',
---
+++ src/islands/ReactHookFormWithZod/index.tsx
@@ -0,0 +1,7 @@
+import { useEffect } from 'react';
+
+function ReactHookFormsWithZodIsland() {
+  return <>야!</>;
+}
+
+export default ReactHookFormsWithZodIsland;
