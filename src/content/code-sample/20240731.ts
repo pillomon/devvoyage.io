6320af5b5b9708dc6439433b5329c530c0066627
Pillsang SUNG <44804024+pillomon@users.noreply.github.com>
2024-11-03 22:36:15 +0900

    Article writing (#2)
    feat: virtualization 글 작성

---
+++ src/content/code-sample/20240731.ts
@@ -0,0 +1,17 @@
+export const SAMPLE_1 = `
+const openLink = (url: string) => {
+  if (typeof window === 'undefined') return;
+
+  window.open(url, '_blank');
+};
+`;
+export const SAMPLE_2 = `
+const popup = () => window.open('', '_blank');
+
+if (!popup) return;
+
+const data = await getURL();
+
+if (data.url) popup.location.href = data.landingUrl;
+else popup.close();
+`;
